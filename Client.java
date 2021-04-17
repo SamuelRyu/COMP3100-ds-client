cea3c40e0145c79ce3e36fbcebbffc3dc9e63216
jonoteo123 <jonathan.kh.teo1@gmail.com>
2021-04-17 19:40:16 +1000

    Integrated XML Parser into main Client
    Dynamic retrieval of system xml

--- Client.java
+++ Client.java
@@ -1,6 +1,13 @@
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.List;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 class Client{
 
@@ -29,6 +36,38 @@ class Client{
             e.printStackTrace();
         }
         return message;
+    }
+
+    public static String XMLFileParser() {
+
+        List<Object[]> servlist = new ArrayList<>();
+
+        try {
+            File inputFile = new File("./ds-system.xml");
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(inputFile);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList servernodelist = doc.getElementsByTagName("server");
+
+            for (int i = 0; i < servernodelist.getLength(); i++) {
+                Node node = servernodelist.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element tElement = (Element) node;
+                    servlist.add(new Object[] { tElement.getAttribute("type"), tElement.getAttribute("coreCount") });
+                }
+            }
+        }
+
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return servlist.get(7)[0].toString();
     }
 
     public static void performHandshake(DataInputStream din, DataOutputStream dout){
